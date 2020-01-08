69f72f798441b8674b0a48649a6044e6af088aa6
lewisre <lewisre@PC1152>
2020-01-08 12:02:55 +0000

    remove console log

--- src/Service.Host/Startup.cs
+++ src/Service.Host/Startup.cs
@@ -63,8 +63,6 @@ namespace Linn.Tax.Service.Host
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
-            loggerFactory.AddConsole();
-
             app.UseCors(builder => builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
 
             if (env.IsDevelopment())
