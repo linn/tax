b32aeea7e8597e5f99d6a58bdea8529385322e5d
Lewis Renfrew <lewis.renfrew@linn.co.uk>
2021-04-27 15:12:25 +0100

    include some pva values in test

--- tests/Unit/Domain.Tests/VatReturnCalculationServiceTests/WhenCalculatingVatReturn.cs
+++ tests/Unit/Domain.Tests/VatReturnCalculationServiceTests/WhenCalculatingVatReturn.cs
@@ -19,7 +19,7 @@
                 2031905.52m,
                 338770.88m,
                 -12257.17m,
-                0.0m, // todo
+                15950.10m,
                 1206741m,
                 103903.25m,
                 20780.65m);
@@ -29,10 +29,10 @@
         public void ShouldReturnVatReturnWithCorrectValues()
         {
             this.result.Should().BeOfType<VatReturn>();
-            this.result.VatDueSales.Should().Be(172505.07m);
+            this.result.VatDueSales.Should().Be(188455.17m);
             this.result.VatDueAcquisitions.Should().Be(0m);
             this.result.TotalVatDue.Should().Be(193285.72m);
-            this.result.VatReclaimedCurrPeriod.Should().Be(347294.36m);
+            this.result.VatReclaimedCurrPeriod.Should().Be(331344.26m);
             this.result.NetVatDue.Should().Be(154008.64m);
             this.result.TotalValueSalesExVat.Should().Be(3639426m);
             this.result.TotalValuePurchasesExVat.Should().Be(2031906m);
